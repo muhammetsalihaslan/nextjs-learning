bcac6892bc9554856894a321fd77934e4f6b84db
salih <muhammetsalihaslan@gmail.com>
2023-06-02 19:07:11 +0300

    footer css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>All right reserved</div>
-      <div></div>
+      <div>
+        <div className={styles.social}>
+          <Image
+            src="/1.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="fimage"
+          />
+          <Image
+            src="/2.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="timage"
+          />
+          <Image
+            src="/3.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="iimage"
+          />
+          <Image
+            src="/4.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="limage"
+          />
+        </div>
+      </div>
     </div>
   );
 };
