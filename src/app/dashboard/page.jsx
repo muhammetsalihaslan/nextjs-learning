57626ba0bcade7133438fa9e068212de75a2d66f
salih <muhammetsalihaslan@gmail.com>
2023-06-06 23:41:21 +0300

    end

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+
   return <div>Dashboard</div>;
 };
 
