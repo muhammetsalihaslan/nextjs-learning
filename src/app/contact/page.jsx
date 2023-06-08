9f41209ea66dd4d14563c52c21fa19ca82ae40df
salih <muhammetsalihaslan@gmail.com>
2023-06-08 18:44:08 +0300

    MetaData

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,11 @@ import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
 
+export const metadata = {
+  title: "NextLearning Contact Information",
+  description: "This is Contact page",
+};
+
 const Contact = () => {
   return (
     <div className={styles.container}>
