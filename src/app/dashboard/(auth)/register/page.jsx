e3c6ad2ddd53655677de1c3f1bc8322ffb004115
salih <muhammetsalihaslan@gmail.com>
2023-06-12 21:46:54 +0300

    register

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[0].value;
+    const password = e.target[0].value;
+
+    try {
+      const res = await fetch("/api/auth/register");
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        {err && "Something went wrong!"}
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/login">
+        Login with an existing account
+      </Link>
+    </div>
+  );
 };
 
 export default Register;
