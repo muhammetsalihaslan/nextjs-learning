49ee2c81ede6b73d94d646ca25ed70b1cce4ad63
salih <muhammetsalihaslan@gmail.com>
2023-06-12 18:03:05 +0300

    google authentication

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,14 @@
+"use client";
+import { signIn } from "next-auth/react";
 import React from "react";
+import styles from "./page.module.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <button onClick={() => signIn("google")}>Login with Google</button>
+    </div>
+  );
 };
 
 export default Login;
